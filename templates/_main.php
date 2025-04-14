d4c16d44bd4b034371c6af3046e5aefb1c673aba
Thomas <evers.thomas@gmail.com>
2025-04-14 16:02:41 +0200

    Update _main.php

--- templates/_main.php
+++ templates/_main.php
@@ -15,41 +15,50 @@
 $home = $pages->get('/'); /** @var HomePage $home */
 
 ?><!DOCTYPE html>
-<html lang="en">
+<html lang="de">
 	<head id="html-head">
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 		<title><?php echo $page->title; ?></title>
 		<link rel="stylesheet" type="text/css" href="<?php echo $config->urls->templates; ?>styles/main.css" />
-		<script src="<?php echo $config->urls->templates; ?>scripts/main.js"></script>
+		
 	</head>
 	<body id="html-body">
-
-		<p id="topnav">
-			<?php echo $home->and($home->children)->implode(" / ", "<a href='{url}'>{title}</a>"); ?>
-		</p>
-		
-		<hr />
-		
-		<h1 id="headline">
-			<?php if($page->parents->count()): // breadcrumbs ?>
-				<?php echo $page->parents->implode(" &gt; ", "<a href='{url}'>{title}</a>"); ?> &gt;
-			<?php endif; ?>
-			<?php echo $page->title; // headline ?>
-		</h1>
-		
-		<div id="content">
-			Default content
-		</div>
+	    <a class="skip-link" href="#main-content">Zum Hauptinhalt springen</a>
+	    
+	    <header role="banner">
+	        <div class="container">
+			<h1><?= $page->title; ?></h1>
+			<p>page subtitle</p>
+	        </div>
+	    </header>
 	
-		<?php if($page->hasChildren): ?>
-		<ul> 
-			<?php echo $page->children->each("<li><a href='{url}'>{title}</a></li>"); // subnav ?>
-		</ul>	
-		<?php endif; ?>
-		
-		<?php if($page->editable()): ?>
-		<p><a href='<?php echo $page->editUrl(); ?>'>Edit this page</a></p>
-		<?php endif; ?>
+	    <nav id="main-nav" role="navigation" aria-label="Hauptnavigation">
+	        <div class="container">
+	            <!-- templatepart navigation -->
+	        </div>
+	    </nav>
 	
+	    <main id="main-content" role="main">
+	        <div class="container">
+	            <article id="content">
+	                <h2><?= $page->title; ?></h2>
+	                <?= $body->body; ?>
+	            </article>
+	        </div>
+	    </main>
+	
+	    <aside role="complementary">
+	        <div class="container">
+	            
+	        </div>
+	    </aside>
+	
+	    <footer role="contentinfo">
+	        <div class="container">
+	            
+	        </div>
+	    </footer>
+	<!-- javascript -->
+	<script src="<?php echo $config->urls->templates; ?>scripts/main.js"></script>
 	</body>
 </html>
