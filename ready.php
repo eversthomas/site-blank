5695594bf946ed77fd68e61545b9fa694f7d6f1e
Thomas <evers.thomas@gmail.com>
2025-04-14 16:18:37 +0200

    Update ready.php

--- ready.php
+++ ready.php
@@ -13,3 +13,25 @@ if(!defined("PROCESSWIRE")) die();
  * copy of all ProcessWire API variables.
  *
  */
+
+/** @var Scss $scss */
+$scss = $this->wire->modules->get('Scss');
+
+// watch all files in /site/templates/scss
+$watchFiles = $files->find(
+  $config->paths->templates . "styles",
+  ['extensions' => 'scss']
+);
+
+// set the import path so that relative @import statements work
+$scss->compiler()->setImportPaths($config->paths->templates . "styles");
+
+// compile SCSS to CSS if any of the watched files changed
+$scss->compileIfChanged(
+  // create this file with the content from the uikit docs:
+  // https://getuikit.com/docs/sass#how-to-build
+  input: $config->paths->templates . "styles/main.scss",
+  watch: $watchFiles,
+  output: $config->paths->templates . "styles/main.css",
+  style: 'compressed',
+);
